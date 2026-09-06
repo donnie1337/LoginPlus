7dd509080b3cc23c6940f140df090243dce1650e
mattyisherenow <109880416+mattyisherenow@users.noreply.github.com>
2026-09-06 02:40:03 -0300

    feat: wire cryptographic premium authentication into plugin

--- src/main/java/com/authsystem/AuthSystem.java
+++ src/main/java/com/authsystem/AuthSystem.java
@@ -4,28 +4,33 @@ import com.authsystem.commands.LoginCommand;
 import com.authsystem.commands.RegisterCommand;
 import com.authsystem.listeners.AntiBypassListener;
 import com.authsystem.listeners.AuthListener;
+import com.authsystem.listeners.PremiumVerificationListener;
 import com.authsystem.manager.LoginProtection;
 import com.authsystem.manager.PlayerDataManager;
 import com.authsystem.manager.SessionManager;
+import com.authsystem.util.PremiumAuthenticator;
+import com.authsystem.util.PremiumLoginVerifier;
+import com.github.retrooper.packetevents.PacketEvents;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AuthSystem extends JavaPlugin {
-
     private static AuthSystem instance;
-
     private PlayerDataManager playerDataManager;
     private SessionManager sessionManager;
     private LoginProtection loginProtection;
+    private PremiumAuthenticator premiumAuthenticator;
+    private PremiumLoginVerifier premiumLoginVerifier;
 
     @Override
     public void onEnable() {
         instance = this;
-
         saveDefaultConfig();
 
         this.playerDataManager = new PlayerDataManager(this);
         this.sessionManager = new SessionManager();
         this.loginProtection = new LoginProtection();
+        this.premiumAuthenticator = new PremiumAuthenticator();
+        this.premiumLoginVerifier = new PremiumLoginVerifier();
 
         getCommand("login").setExecutor(new LoginCommand(this));
         getCommand("registro").setExecutor(new RegisterCommand(this));
@@ -33,7 +38,10 @@ public class AuthSystem extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new AuthListener(this), this);
         getServer().getPluginManager().registerEvents(new AntiBypassListener(this), this);
 
-        getLogger().info("AuthSystem ativado com sucesso!");
+        PacketEvents.getAPI().getEventManager().registerListener(
+                new PremiumVerificationListener(this, premiumLoginVerifier, premiumAuthenticator));
+
+        getLogger().info("AuthSystem ativado com autenticacao premium criptografica!");
     }
 
     @Override
@@ -44,19 +52,9 @@ public class AuthSystem extends JavaPlugin {
         getLogger().info("AuthSystem desativado.");
     }
 
-    public static AuthSystem getInstance() {
-        return instance;
-    }
-
-    public PlayerDataManager getPlayerDataManager() {
-        return playerDataManager;
-    }
-
-    public SessionManager getSessionManager() {
-        return sessionManager;
-    }
-
-    public LoginProtection getLoginProtection() {
-        return loginProtection;
-    }
+    public static AuthSystem getInstance() { return instance; }
+    public PlayerDataManager getPlayerDataManager() { return playerDataManager; }
+    public SessionManager getSessionManager() { return sessionManager; }
+    public LoginProtection getLoginProtection() { return loginProtection; }
+    public PremiumAuthenticator getPremiumAuthenticator() { return premiumAuthenticator; }
 }
